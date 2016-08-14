a41ce19ecf2b13434578ac6032733f0d1d3bf6b2
steveYeah <hutchinsteve@gmail.com>
2016-08-14 19:07:59 +0100

    use config file fixture instead of specifing name

--- test/unit/cli/test_read_config.py
+++ test/unit/cli/test_read_config.py
@@ -5,11 +5,8 @@ from amqpeek import cli
 
 class TestReadConfig(object):
 
-    test_config_name = 'test_config.yaml'
-
-    @pytest.mark.usefixtures('config_file')
-    def test_read_config_returns_correct_data(self, config_data):
-        result = cli.read_config(self.test_config_name)
+    def test_read_config_returns_correct_data(self, config_data, config_file):
+        result = cli.read_config(config_file)
 
         assert result == config_data
 
