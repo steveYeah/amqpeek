6339809edc6b1f8dbbba942e3f4814d43ff0bc09
steveYeah <hutchinsteve@gmail.com>
2016-08-21 17:54:12 +0100

    Add config file Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
+#!/usr/bin/env python
+from codecs import open
 import os
 from setuptools import setup, find_packages
 
+
 here = os.path.abspath(os.path.dirname(__file__))
 
-# with open(os.path.join(here, 'README.rst'), mode='r', encoding='utf-8') as handle:
-#     readme = handle.read()
+with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as handle:
+    readme = handle.read()
+
+config_path = '{}/.amqpeek/'.format(os.path.expanduser("~"))
 
 setup(
     name='amqpeek',
     description='RMQ CLI Monitor',
     packages=find_packages(exclude=['test', 'test.*']),
     version='0.0.1',
-    long_description='bob',
+    long_description=readme,
     license="MIT",
     author='Steve Hutchins',
     author_email='hutchinsteve@gmail.com',
     url='https://github.com/steveYeah/AmqPeek',
-    download_url='https://github.com/steveYeah/AmqPeek/archive/v0.0.1.tar.gz',
+    download_url='https://github.com/steveYeah/amqpeek/archive/v0.0.1.tar.gz',
     keywords=(
         'amqp',
         'monitor',
@@ -26,7 +31,7 @@ setup(
         'notifications'
     ),
     data_files=(
-        ('/etc', ['config/amqpeek.yaml'])
+        [(config_path, ['config/amqpeek.yaml'])]
     ),
     install_requires=[
         'click',
